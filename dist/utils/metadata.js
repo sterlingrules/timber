048f31bb3a9037e52573a249a1f1f18106981725
sterlingrules <me@mynameissterling.com>
2017-12-31 11:12:49 -0500

    update repo with clean clone

--- dist/utils/metadata.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var stripMetadata = exports.stripMetadata = function stripMetadata(log) {
-  return log.split(' @metadata')[0] + '\n';
-};
